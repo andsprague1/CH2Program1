81d226c5731da8f8330b5e2f83fafcc897d037c5
andsprague1 <andsprague1@gmail.com>
2025-09-17 18:34:24 -0400

    Whopse didn't commit

--- CH2Program1/CH2Program1.cpp
+++ CH2Program1/CH2Program1.cpp
@@ -8,6 +8,39 @@ int main()
 {
     int digit1, digit2,
         digit3, digit4, input;
+    /*We want to be given a number 
+    of farthings
+    turn those farthings into 
+    the number of pounds,
+    the number of penies,
+    the number of shillings
+    and farthings remaing
+    favoriting the largest currency
+    */
+    int coins;
+    int farthings, pennies, shillings, pounds;
+    cout << "How many farthings do you have sir?";
+    //get the totatal farthings
+    cin >> coins;
+    //modulo 4
+    farthings = coins % 4;
+    //divide it by 4 and set nextcoins to that
+    coins = coins / 4;//converting to pennies
+        //take next coins and modulo 12 for pennies
+    pennies = coins % 12;
+    //divide by 12
+    coins = coins / 12;
+    //take that and modulo 20 to get shillings
+    shillings = coins % 20;
+    pounds = coins / 20;
+
+    cout << "YOU have :" << pounds << " Pounds\n"
+        << shillings << " Shillings\n"
+        << pennies << " Pennies\n"
+        << "and" << farthings << " Farthings\n"
+        << "good sir!";
+    //divide that by 20, that's the biggest so it's what's left
+
 
     return 0;
 
